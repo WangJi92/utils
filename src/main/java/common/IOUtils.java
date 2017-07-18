3f1b10330308d753b400eee1ba07207d47d560fe
汪吉 <983433479@qq.com>
2017-07-18 14:48:06 +0800

    [add] IO操作类，进行各种字节流和字符流之间的复制和处理.

--- src/main/java/common/IOUtils.java
+++ src/main/java/common/IOUtils.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.io.*;
+import java.nio.channels.Channel;
 
 /**
  * descrption: IO工具类
@@ -116,6 +117,131 @@ public class IOUtils {
         return toString(input,DEFAULT_BUFFER_SIZE);
     }
 
+
+  //InputStream => byte[]
+    /**
+     * @desction: 将inputStream转换为 byte[]数组，借助于ByteArrayOutputSream，然后InputStram 向outputSream复制
+     * @author: wangji
+     * @date: 2017/7/17
+     * @param: [input]
+     * @return: byte[]
+     */
+    public static byte[] toByteArrays(final  InputStream input)throws IOException{
+
+       return toByteArrays(input,DEFAULT_BUFFER_SIZE);
+    }
+    public static byte[] toByteArrays(final  InputStream input,int bufferSize) throws IOException{
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        //这里的ByteArrayOutputSream 和ByteArrayInputSream 就是byte[] 和流之间的装饰类
+        //类似的还有CharArrayReader 和 CharArrayWriter 只不过是一个是对于字节流一个是对于字符流之间的处理
+        //将输入流复制到输出流然后在处理，将内部的byte[]数据输出
+        copy(input,output,bufferSize);
+        return output.toByteArray();
+    }
+
+   //Reader  ==> * (inputSream,String)
+
+
+    //reader = > inputStream
+
+    public static  void copy(final Reader reader,final OutputStream output)throws IOException{
+        //字符流向字节流转换
+        copy(reader,output,DEFAULT_BUFFER_SIZE);
+    }
+    public static  void copy(final Reader reader,final OutputStream output,int bufferSize)throws IOException{
+        //字符流向字节流转换,需要借助中间转换，将outputSream 转为 outputSreamWriter
+        final OutputStreamWriter outputStreamWriter  = new OutputStreamWriter(output,DEAULT_ENCODING);
+        copy(reader,outputStreamWriter);
+        outputStreamWriter.flush();
+
+    }
+
+    //reader（Char[]） =>String
+    public static String toString(Reader reader) throws IOException {
+        return toString(reader, DEFAULT_BUFFER_SIZE);
+    }
+
+    //reader =>StringWriter(复制) =>String
+    public static String toString(Reader reader, int bufferSize) throws IOException {
+        final StringWriter writer = new StringWriter();//和其他的有异曲同工值妙
+        //通过中间进行转换
+        copy(reader, writer, bufferSize);
+        return writer.toString();
+    }
+
+    //reader => byte[](字符流转换为字节流)
+    public  static byte[] toByteArrays(final Reader reader)throws IOException{
+        return toByteArrays(reader,DEFAULT_BUFFER_SIZE);
+    }
+    public  static byte[] toByteArrays(final Reader reader,int bufferSize)throws IOException{
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();//生成字节流 和fileInputStream 意义类似
+        final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream);
+        //Reader -> OutPutSreamWriter(ByteArrayOutputSream);
+        copy(reader,outputStreamWriter,bufferSize);
+        outputStreamWriter.flush();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+
+    //String === *
+
+    //String ===>Outputstream
+    public static void copy(final String  inputString,final OutputStream output) throws IOException{
+      copy(inputString,output);
+    }
+    public static void copy(final String  inputString,final OutputStream output,int bufferSize) throws IOException{
+        StringReader reader = new StringReader(inputString);
+        //包装一下子,String =>Reader
+        //包装一下子 Outputstream => OutputStreamWriter
+        //虽然包装了，但是最后的实际写入还是在OutputSream中的
+        final  OutputStreamWriter outputStreamWriter = new OutputStreamWriter(output);
+        copy(reader,outputStreamWriter,bufferSize);
+        outputStreamWriter.flush();
+    }
+
+
+    //String =>byte[]
+
+    public static byte[] toByteArrays(final String string) throws IOException{
+        return string.getBytes();
+    }
+    public static byte[] toByteArrays(final String string,int bufferSize) throws IOException {
+        final StringReader stringReader = new StringReader(string);
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream);
+        copy(stringReader,outputStreamWriter,bufferSize);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    //byte[] => *
+
+    //byte ->Writer
+
+    /**
+     * 1.byte首先转换为字节流
+     * 2.字节流转为字符流Reader
+     * 3.字符流Reader复制到Writer
+     * @param inputByteArray
+     * @param writer
+     * @throws IOException
+     */
+    public static void copy(final byte[] inputByteArray, final Writer writer) throws IOException {
+        copy(inputByteArray, writer, DEFAULT_BUFFER_SIZE);
+    }
+
+    public static void copy(final byte[] inputByteArray, final Writer writer, int bufferSize) throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(inputByteArray);
+        final InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream);
+        copy(inputStreamReader, writer, bufferSize);
+    }
+
+    //byte[]=> outputstream
+    public static void copy(final  byte[] inputByteArray,final OutputStream outputStream)throws IOException{
+        outputStream.write(inputByteArray);
+    }
+
+
+
     /**
      * @desction: 关闭异常，关闭失败不做任何处理
      * @author: wangji
@@ -131,6 +257,54 @@ public class IOUtils {
             in.close();
         } catch (IOException e) {
            //关闭异常不做任务处理
+        }
+    }
+
+    public static void close(Channel channel) {
+        if (channel == null) {
+            return;
+        }
+
+        try {
+            channel.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+
+    public static void close(OutputStream outputStream) {
+        if (outputStream == null) {
+            return;
+        }
+
+        try {
+            outputStream.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+
+    public static void close(Reader reader) {
+        if (reader == null) {
+            return;
+        }
+
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+
+    public static void close(Writer writer) {
+        if (writer == null) {
+            return;
+        }
+
+        try {
+            writer.close();
+        } catch (IOException ex) {
+            // ignore
         }
     }
 }
